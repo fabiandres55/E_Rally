faf50450610617794c7128b9bb17184710d29f05
Andrew <66757460+fabiandres55@users.noreply.github.com>
2025-02-09 12:25:15 -0500

    Summary_3

---
+++ Autoload/Datos_pendinete_2D/Pendiente.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var progress = 2  # aqui intro: 0, intro+rally_1 : 1, intro+rally_1,rally_2 : 2. 
+
+var motor_propieties = 0
+
+var motor_choose_trueorfalse
+
+var Audio_time = 0
+
+var portales_secuancia_logica = 0
